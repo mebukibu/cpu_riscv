0dc649d41a67ce6c34fbea0f51c26ddc047d747a
mebukibu <masa1280g@gmail.com>
2023-08-17 20:01:26 +0900

    UART用コントローラを追加 UART用レジスタに代入する値を修正（rdata -> wdata_mem）

--- src/uart.v
+++ src/uart.v
@@ -3,22 +3,35 @@
 module uart (
   input wire clk,
   input wire rst_n,
-  output wire intr,
-  input wire [`WORD_LEN-1:0] rdata,
+  output reg intr,
   input wire wen_mem,
   input wire [`WORD_LEN-1:0] addr_d_mem,
   input wire [`WORD_LEN-1:0] wdata_mem,
-  output wire wen_uart,
-  output wire [`WORD_LEN-1:0] addr_d_uart,
-  output wire [`WORD_LEN-1:0] wdata_uart
+  output reg [`WORD_LEN-1:0] addr_d_uart,
+  output reg [`WORD_LEN-1:0] wdata_uart,
+  output wire uart_out
 );
-
-  // Debug
-  assign intr = 0;
 
   // register
   reg [`UART_LEN-1:0] uart_regfile [0:`UART_LEN-1];
   integer i;
+
+  // For transmitter
+  reg [`UART_LEN-1:0] data_in;
+  reg wen_trans;
+  wire busy;
+
+  //**********************************
+  // transmitter
+
+  transmitter transmitter0 (
+    .clk(clk),
+    .rst_n(rst_n),
+    .data_in(data_in),
+    .we(wen_trans),
+    .data_out(uart_out),
+    .busy(busy)
+  );
 
 
   //**********************************
@@ -34,12 +47,46 @@ module uart (
     else if (wen_mem & ((addr_d_mem & ~`WORD_LEN'b111) == `UART_ADDR)) begin
       case (addr_d_mem[2])
         1'b0 : for (i = 0; i < `WORD_LEN / `UART_LEN; i = i + 1) begin
-                uart_regfile[i]   <= rdata[i*`UART_LEN +: `UART_LEN];
+                uart_regfile[i]   <= wdata_mem[i*`UART_LEN +: `UART_LEN];
                end
         1'b1 : for (i = 0; i < `WORD_LEN / `UART_LEN; i = i + 1) begin
-                uart_regfile[i+4] <= rdata[i*`UART_LEN +: `UART_LEN];
+                uart_regfile[i+4] <= wdata_mem[i*`UART_LEN +: `UART_LEN];
                end
       endcase
+    end
+  end
+
+
+  //**********************************
+  // Controller
+
+  always @(posedge clk, negedge rst_n) begin
+    if (!rst_n) begin
+      intr <= 1'b0;
+      addr_d_uart <= `WORD_LEN'b0;
+      wdata_uart <= `WORD_LEN'b0;
+      wen_trans <= 1'b0;
+      data_in <= `UART_LEN'b0;
+    end
+    else if (uart_regfile[`UART_LSR] & `LSR_TX_IDLE) begin
+      if (wen_mem & (addr_d_mem == `UART_ADDR)) begin
+        intr <= 1'b1;
+        addr_d_uart <= `UART_ADDR + `UART_LSR;
+        wdata_uart <= {uart_regfile[7], uart_regfile[6], (uart_regfile[`UART_LSR] & ~`LSR_TX_IDLE), uart_regfile[4]};
+        wen_trans <= 1'b1;
+        data_in <= wdata_mem[`UART_LEN-1:0];
+      end
+      else begin
+        intr <= 1'b0;
+      end
+    end
+    else begin
+      wen_trans <= 1'b0;
+      if (!busy) begin
+        intr <= 1'b1;
+        addr_d_uart <= `UART_ADDR + `UART_LSR;
+        wdata_uart <= {uart_regfile[7], uart_regfile[6], (uart_regfile[`UART_LSR] | `LSR_TX_IDLE), uart_regfile[4]};
+      end
     end
   end
   
--- tb/uart_tb.v
+++ tb/uart_tb.v
@@ -5,13 +5,12 @@ module uart_tb ();
   reg clk;
   reg rst_n;
   wire intr;
-  reg [`WORD_LEN-1:0] rdata;
   wire wen_mem;
   wire [`WORD_LEN-1:0] addr_d_mem;
   wire [`WORD_LEN-1:0] wdata_mem;
-  wire wen_uart;
   wire [`WORD_LEN-1:0] addr_d_uart;
   wire [`WORD_LEN-1:0] wdata_uart;
+  wire uart_out;
 
   reg wen_core;
   reg [`WORD_LEN-1:0] addr_d_core;
@@ -19,7 +18,7 @@ module uart_tb ();
 
   uart uart0 (.*);
 
-  assign wen_mem    = intr ? wen_uart    : wen_core;
+  assign wen_mem    = intr ? intr        : wen_core;
   assign addr_d_mem = intr ? addr_d_uart : addr_d_core;
   assign wdata_mem  = intr ? wdata_uart  : wdata_core;
 
@@ -28,11 +27,21 @@ module uart_tb ();
 
   initial begin
     $dumpvars;
-    rst_n = 0; wen_core = 0; #20;
+    rst_n = 0; wen_core = 0; addr_d_core = `WORD_LEN'h8000; wdata_core = `WORD_LEN'h0; #20;
     rst_n = 1; #5;
-    wen_core = 1; addr_d_core = `WORD_LEN'h1004; rdata = `WORD_LEN'h0001; #10;
+    #11;
+    addr_d_core = `WORD_LEN'h1000; wdata_core = `WORD_LEN'h0041; #9;
+    wen_core = 1; #10
     $display("uart_regfile[0] = 0x%h", uart0.uart_regfile[0]);
-    $display("uart_regfile[4] = 0x%h", uart0.uart_regfile[4]);
+    $display("uart_regfile[5] = 0x%h", uart0.uart_regfile[5]);
+    #10;
+    $display("uart_regfile[0] = 0x%h", uart0.uart_regfile[0]);
+    $display("uart_regfile[5] = 0x%h", uart0.uart_regfile[5]);
+    #10;
+    wait (uart0.busy == 1'b0);
+    #30;
+    $display("uart_regfile[0] = 0x%h", uart0.uart_regfile[0]);
+    $display("uart_regfile[5] = 0x%h", uart0.uart_regfile[5]);
     #10;
     $finish;
   end
