e1ca873393e7c3df7a295535775615668c03e38f
mebukibu <masa1280g@gmail.com>
2023-08-18 16:24:20 +0900

    printfの機能を強化 16進数，文字列を表示できるように 10進数は除算が必要なため実装していない

--- os/kernel/main.c
+++ os/kernel/main.c
@@ -3,7 +3,13 @@
 void
 main()
 {
-  printf("Hello World!\n");
+  char s[] = "Number";
+  int i;
+  for (i = 0; i < 5; i++)
+    printf("%s : %x\n", s, i);
+  
+  printf("%%, %h", 1);
+
   while (1)
     asm volatile("unimp");
 }
--- os/kernel/printf.c
+++ os/kernel/printf.c
@@ -1,6 +1,33 @@
 #include <stdarg.h>
 
+#include "types.h"
 #include "defs.h"
+
+static char digits[] = "0123456789abcdef";
+
+static void
+printhex(int xx, int sign)
+{
+  char buf[16];
+  int i;
+  uint x;
+
+  if(sign && (sign = xx < 0))
+    x = -xx;
+  else
+    x = xx;
+
+  i = 0;
+  do {
+    buf[i++] = digits[x & 0xf];
+  } while((x >>= 4) != 0);
+
+  if(sign)
+    buf[i++] = '-';
+
+  while(--i >= 0)
+    consputc(buf[i]);
+}
 
 // Print to the console. only understands %d, %x, %p, %s.
 void
@@ -9,7 +36,7 @@ printf(char *fmt, ...)
   va_list ap;
   int i, c;
   // int locking;
-  // char *s;
+  char *s;
 
   // locking = pr.locking;
   // if(locking)
@@ -24,34 +51,34 @@ printf(char *fmt, ...)
       consputc(c);
       continue;
     }
-    // c = fmt[++i] & 0xff;
-    // if(c == 0)
-    //   break;
-    // switch(c){
+    c = fmt[++i] & 0xff;
+    if(c == 0)
+      break;
+    switch(c){
     // case 'd':
     //   printint(va_arg(ap, int), 10, 1);
     //   break;
-    // case 'x':
-    //   printint(va_arg(ap, int), 16, 1);
-    //   break;
+    case 'x':
+      printhex(va_arg(ap, int), 1);
+      break;
     // case 'p':
     //   printptr(va_arg(ap, uint64));
     //   break;
-    // case 's':
-    //   if((s = va_arg(ap, char*)) == 0)
-    //     s = "(null)";
-    //   for(; *s; s++)
-    //     consputc(*s);
-    //   break;
-    // case '%':
-    //   consputc('%');
-    //   break;
-    // default:
-    //   // Print unknown % sequence to draw attention.
-    //   consputc('%');
-    //   consputc(c);
-    //   break;
-    // }
+    case 's':
+      if((s = va_arg(ap, char*)) == 0)
+        s = "(null)";
+      for(; *s; s++)
+        consputc(*s);
+      break;
+    case '%':
+      consputc('%');
+      break;
+    default:
+      // Print unknown % sequence to draw attention.
+      consputc('%');
+      consputc(c);
+      break;
+    }
   }
   va_end(ap);
 
