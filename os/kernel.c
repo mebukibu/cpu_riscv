be4055d8f91c674288c4dbe684c1e25796f0756f
mebukibu <masa1280g@gmail.com>
2024-02-24 16:34:32 +0900

    10 メモリ割り当て

--- os/kernel.c
+++ os/kernel.c
@@ -1,7 +1,21 @@
 #include "kernel.h"
 #include "common.h"
 
-extern char __bss[], __bss_end[], __stack_top[];
+extern char __stack_top[];
+extern char __bss[], __bss_end[];
+extern char __free_ram[], __free_ram_end[];
+
+paddr_t alloc_pages(uint32_t n) {
+  static paddr_t next_paddr = (paddr_t) __free_ram;
+  paddr_t paddr = next_paddr;
+  next_paddr += n * PAGE_SIZE;
+
+  if (next_paddr > (paddr_t) __free_ram_end)
+    PANIC("out of memory");
+
+  memset((void *) paddr, 0, n * PAGE_SIZE);
+  return paddr;
+}
 
 __attribute__((naked))
 __attribute__((aligned(4)))
@@ -95,7 +109,13 @@ void kernel_main(void) {
   printf("\n\n");
 
   WRITE_CSR(stvec, (uint32_t) kernel_entry);
-  __asm__ __volatile__("unimp");
+
+  paddr_t paddr0 = alloc_pages(2);
+  paddr_t paddr1 = alloc_pages(1);
+  printf("alloc_pages test: paddr0=%p\n", paddr0);
+  printf("alloc_pages test: paddr1=%p\n", paddr1);
+
+  PANIC("booted!");
 }
 
 __attribute__((section(".text.boot")))
